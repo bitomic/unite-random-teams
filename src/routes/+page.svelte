e7698daeaba6fe2d9c5e4b065cfd8864cdcf68c0
bitomic <35199700+bitomic@users.noreply.github.com>
2023-07-13 23:26:10 -0600

    fix: remove useless messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { players } from '$lib/stores/matchmaking'
     import Team from '$lib/components/Team.svelte'
-    import TopBanner from '$lib/components/TopBanner.svelte'
 
 	$players.clear()
 
@@ -36,8 +35,6 @@
 	<title> Crea equipos aleatorios | Pokémon UNITE </title>
 </svelte:head>
 
-<TopBanner />
-
 <div class="room">
 	<Team team={ $players.team1 } teamNumber={ 1 } changeAvatar={ changeAvatar } changePokemon={ changePokemon } />
 	<div class="room__separator">
@@ -47,13 +44,6 @@
 </div>
 
 <div class="columns">
-	<div class="column soon">
-		<h2> Próximamente... </h2>
-		<ul>
-			<li> Cambia todos los Pokémon a la vez. </li>
-			<li> Aplica filtros para las opciones de Pokémon (solo permitir cierto rol, solo permitir ciertas opciones, evitar que se repitan Pokémon entre ambos equipos). </li>
-		</ul>
-	</div>
 	<div class="column buttons">
 		<button class="btn" on:click={ randomize }> Cambiar equipos al azar </button>
 		<button class="btn" on:click={ randomizePokemon }> Cambiar todos los Pokémon </button>
