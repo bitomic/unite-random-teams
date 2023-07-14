806a6bd19733e5d9c207c726834f9328333b2670
bitomic <35199700+bitomic@users.noreply.github.com>
2023-07-13 22:44:31 -0600

    refactor: split some css and components

---
+++ src/lib/components/TopBanner.svelte
@@ -0,0 +1,13 @@
+<ul class="infobanner">
+	<li> Puedes cambiar el nombre de cada personaje escribiendo en el cuadro debajo de su avatar. </li>
+	<li> Haz click en el personaje para cambiar su avatar. </li>
+	<li> Puedes hacer click en el Pokémon para obtener uno nuevo al azar. </li>
+</ul>
+
+<style>
+.infobanner {
+	color: #fff;
+	margin: 1em auto;
+	width: 80%;
+}
+</style>
