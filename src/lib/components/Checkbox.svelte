03946641dd9c2518f5b357d56cae0621a6669f88
bitomic <35199700+bitomic@users.noreply.github.com>
2023-07-19 21:19:27 -0600

    feat: checkbox component

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,17 @@
+<div class="checkbox">
+	<input type="checkbox">
+	<label for=""> <slot /> </label>
+</div>
+
+<style>
+.checkbox {
+	align-items: center;
+	column-gap: 0.85em;
+	display: flex;
+	margin: 0.75em 0;
+}
+input {
+	height: 25px;
+	width: 25px;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    import Matchroom from '$lib/components/Matchroom.svelte'
+    import Checkbox from '$lib/components/Checkbox.svelte';
+import Matchroom from '$lib/components/Matchroom.svelte'
     import TextInput from '$lib/components/TextInput.svelte';
     import TwitchIntegrator from '$lib/components/TwitchIntegrator.svelte';
     import { _ } from '$lib/stores/i18n';
@@ -11,7 +12,17 @@
         <Matchroom />
     </div>
     <div class="column column--right">
-        <TwitchIntegrator />
+        <div class="module">
+            <TwitchIntegrator />
+        </div>
+        <div class="module">
+            <h3> { $_.get( 'rules.header' ) } </h3>
+            <div class="checkboxes">
+                <Checkbox> A </Checkbox>
+                <Checkbox> A </Checkbox>
+                <Checkbox> A </Checkbox>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -28,5 +39,10 @@
     border-left: 2px solid #222;
     margin-left: var(--spacing);
     padding-left: var(--spacing);
+}
+.module:not(:first-child) {
+    border-top: 1px solid #222;
+    margin-top: 2em;
+    padding-top: 2em;
 }
 </style>
