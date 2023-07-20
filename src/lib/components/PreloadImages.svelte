7374bbe10d56902c783e192e6040f0ff57ea4595
bitomic <35199700+bitomic@users.noreply.github.com>
2023-07-19 21:21:55 -0600

    feat: preload images in the worst possible way, probably

---
+++ src/lib/components/PreloadImages.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import pokemon from '../../pokemon.json'
+</script>
+
+<div class="preload">
+	{ #each Object.keys( pokemon ) as name }
+		<img src="/roster/{ name }.png" alt={ name }>
+	{ /each }
+</div>
+
+<style>
+.preload {
+	position: relative;
+}
+img {
+	left: 0;
+	opacity: 0;
+	pointer-events: none;
+	position: absolute;
+	top: 0;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import Checkbox from '$lib/components/Checkbox.svelte';
 import Matchroom from '$lib/components/Matchroom.svelte'
+    import PreloadImages from '$lib/components/PreloadImages.svelte';
     import TextInput from '$lib/components/TextInput.svelte';
     import TwitchIntegrator from '$lib/components/TwitchIntegrator.svelte';
     import { _ } from '$lib/stores/i18n';
 
 </script>
+
+<PreloadImages />
 
 <div class="columns">
     <div class="column column--left">
