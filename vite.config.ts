794b33350188e7d4efad766de4c8cce768504132
bitomic <35199700+bitomic@users.noreply.github.com>
2023-08-23 13:54:03 -0600

    fix: util polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig( {
 	// @ts-expect-error - typing mismatch for plugin
 	plugins: [ sveltekit() ],
+	resolve: {
+		alias: {
+			inspect: 'rollup-plugin-node-polyfills/polyfills/util'
+		}
+	},
 	test: {
 		include: [ 'src/**/*.{test,spec}.{js,ts}' ]
 	}
