aa93d2ed7af64e38e90e4a297b2b8c49a408d3c4
bitomic <35199700+bitomic@users.noreply.github.com>
2023-08-23 14:10:09 -0600

    chore: fix polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import rollupNodePolyfill from 'rollup-plugin-node-polyfills'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 export default defineConfig( {
+	build: {
+		rollupOptions: {
+			// @ts-expect-error - typing mismatch for plugin
+			plugins: [ rollupNodePolyfill() ]
+		}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// @ts-expect-error - typing mismatch for plugin
+			plugins: [ NodeModulesPolyfillPlugin() ]
+		}
+	},
 	// @ts-expect-error - typing mismatch for plugin
 	plugins: [ sveltekit() ],
 	resolve: {
 		alias: {
-			inspect: 'rollup-plugin-node-polyfills/polyfills/util'
+			buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+			util: 'rollup-plugin-node-polyfills/polyfills/util'
 		}
 	},
 	test: {
