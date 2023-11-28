981767c67281c9dc1d46c563b9dcc9ec400f9ada
bitomic <35199700+bitomic@users.noreply.github.com>
2023-11-28 15:25:49 -0700

    fix: global unique by default

--- src/lib/client/matchroom/Matchroom.ts
+++ src/lib/client/matchroom/Matchroom.ts
@@ -10,6 +10,7 @@ import sample from 'lodash-es/sample'
 import type { Writable } from 'svelte/store'
 import { UniqueTeamStrategy } from './strategies/UniqueTeam'
 import { BanlistStrategy } from './strategies/Banlist'
+import { GlobalUniqueStrategy } from './strategies/GlobalUnique'
 
 interface MatchroomOptions {
 	removeAndRotate?: boolean
@@ -41,7 +42,8 @@ export class Matchroom {
 	public static store: Writable<Matchroom> | null = null
 	public readonly strategies = new StrategyManager(
 		new UniqueTeamStrategy(),
-		new BanlistStrategy()
+		new BanlistStrategy(),
+		new GlobalUniqueStrategy()
 	)
 	public readonly team1 = new Team()
 	public readonly team2 = new Team()
